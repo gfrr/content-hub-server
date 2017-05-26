9cb130e1bcac4b5c1e0e6c87cd4e56be7a2fdc21
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-05-26 15:58:28 +0200

    saving content 102

--- routes/users.js
+++ routes/users.js
@@ -1,81 +1,68 @@
-const express           = require('express');
-const router            = express.Router();
-const jwt               = require('jsonwebtoken');
-const jwtOptions        = require('../config/jwtOptions');
-const User              = require("../models/user");
-const Content           = require("../models/content");
-const bcrypt            = require("bcrypt");
-const bcryptSalt        = 10;
-const passport          = require("../config/passport.js");
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const jwtOptions = require('../config/jwtOptions');
+const User = require("../models/user");
+const Content = require("../models/content");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const passport = require("../config/passport.js");
 
 
-router.get("/secret", passport.authenticate('jwt', { session: false }), (req, res)=>{
-  res.json({message: "Success! You can not see this without a token"});
+router.get("/secret", passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  res.json({
+    message: "Success! You can not see this without a token"
+  });
 });
 
-router.post('/users/:id/save', passport.authenticate('jwt', { session: false }),
-  (req, res)=>{
-  console.log("SAVE CONTENT CALLED");
-  console.log(req.params.id);
-  console.log(req.body);
-  const newContent = Content(req.body);
-  console.log(newContent);
-  newContent.save((err, content)=>{
-    if(err) res.status(400).json({message: err});
-    else console.log("new item saved");
+router.post('/users/:id/save', passport.authenticate('jwt', {
+    session: false
+  }),
+  (req, res) => {
+    console.log("SAVE CONTENT CALLED");
+    const newContent = Content(req.body);
+    newContent.save((err, content) => {
+      if (err) res.status(400).json({
+        message: err
+      });
+      User.find({
+        _id: req.params.id
+      }, (err, users) => {
+        if (err) res.status(400).json({
+          message: err
+        });
+        users[0].favorites.push(newContent._id);
+        users[0].save();
+        console.log(users[0]);
+        res.status(200).json({
+          message: typeof(users[0].favorites[0])
+        });
+      });
+    });
+
   });
-  
-  // console.log(req.params.id);
-  // console.log(req.body);
-  /*
-  newUser.save((err, user) => {
-    if (err) res.status(400).json({ message: err });
-     else {
-      const payload = {id: user._id, user: user.username};
-      const token = jwt.sign(payload, jwtOptions.secretOrKey);
-      res.status(200).json({message: "ok", token: token, user: user});
-    }
-  });
-  */
-  res.status(200).json("ayy");
-});
+
 
 /* DELETE a USER. */
 router.delete('/users/:id',
-  passport.authenticate('jwt', { session: false }), (req, res)=>{
+  passport.authenticate('jwt', {
+    session: false
+  }), (req, res) => {
 
-  console.log("DELETE CALLED ");
-  User.findOneAndRemove({'_id' : req.params.id}, (err)=>{
-      if(err) res.status(400).json({message: 'Not found'});
-      res.status(200).json({message: 'ELIMINATED'});
+    console.log("DELETE CALLED ");
+    User.findOneAndRemove({
+      '_id': req.params.id
+    }, (err) => {
+      if (err) res.status(400).json({
+        message: 'Not found'
       });
-});
-
-
-
-
-
-// const contentSchema = new Schema({
-//   source: {
-//     type: String,
-//     enum: ["YOUTUBE", "TWITTER", "TUMBLR", "REDDIT"],
-//     default: "YOUTUBE"
-//   },
-//   data: {
-//     type: Object,
-//     default: {}
-//   },
-//   timestamps: {
-//     createdAt: "created_at",
-//     updatedAt: "updated_at"
-//   }
-// });
-
-
-
-
-
-
+      res.status(200).json({
+        message: 'ELIMINATED'
+      });
+    });
+  });
 
 
 module.exports = router;
