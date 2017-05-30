5987c0532f670022b7f2b594f3dc3a8ac22515da
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-05-30 14:47:52 +0200

    slider 101

--- models/search.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
-
-const searchSchema = new Schema({
-
-});
-
-const Search = mongoose.model("Search", searchSchema);
-module.exports = Search;
